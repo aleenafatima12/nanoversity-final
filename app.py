c1787071502f51b4654317ea2e8bfc1a7f3fb7b5
Aleena Fatima <166166492+aleenafatima12@users.noreply.github.com>
2025-05-24 12:42:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -289,13 +289,10 @@ if __name__ == '__main__':
     app.run(debug=True, port=5050)
 
 
+import os
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
-
-from flask import Flask
-
-app = Flask(__name__)
 
 
